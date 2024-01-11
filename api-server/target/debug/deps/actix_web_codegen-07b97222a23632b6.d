503345a0cb2804011cfff6974ad8b09bf179758c
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-01-12 00:11:59 +0900

    add dependencies for actix-web, backend api server

---
+++ api-server/target/debug/deps/actix_web_codegen-07b97222a23632b6.d
@@ -0,0 +1,6 @@
+/Users/suzukitakamasa/Desktop/casaparo/api-server/target/debug/deps/libactix_web_codegen-07b97222a23632b6.dylib: /Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/lib.rs /Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/route.rs
+
+/Users/suzukitakamasa/Desktop/casaparo/api-server/target/debug/deps/actix_web_codegen-07b97222a23632b6.d: /Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/lib.rs /Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/route.rs
+
+/Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/lib.rs:
+/Users/suzukitakamasa/.cargo/registry/src/index.crates.io-6f17d22bba15001f/actix-web-codegen-4.2.2/src/route.rs:
