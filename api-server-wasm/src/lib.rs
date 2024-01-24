450730eb5c8e57925d70fc60f5a69e1a1b3bf2bc
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-01-25 01:11:24 +0900

    mod households and schedules endpoint

--- api-server-wasm/src/lib.rs
+++ api-server-wasm/src/lib.rs
@@ -7,15 +7,21 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     Router::new()
         .get_async("/household", |_, ctx| async move {
+            let year = ctx.param("year").unwrap();
+            let month = ctx.param("month").unwrap();
             let d1 = ctx.env.d1("DB")?;
-            let statement = d1.prepare("select * from households");
-            let result = statement.all().await?;
+            let statement = d1.prepare("select * from households where year = ?1 and month = ?2");
+            let query = statement.bind(&[year.into(), month.into()])?;
+            let result = query.all().await?;
             Response::from_json(&result.results::<models::Household>().unwrap())
         })
         .get_async("/schedule", |_, ctx| async move {
+            let year = ctx.param("year").unwrap();
+            let month = ctx.param("month").unwrap();
             let d1 = ctx.env.d1("DB")?;
-            let statement = d1.prepare("select * from schedules");
-            let result = statement.all().await?;
+            let statement = d1.prepare("select * from schedules where year = ?1 and month = ?2");
+            let query = statement.bind(&[year.into(), month.into()])?;
+            let result = query.all().await?;
             Response::from_json(&result.results::<models::Schedule>().unwrap())
         })
     .run(req, env)
