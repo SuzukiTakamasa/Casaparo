e253f7ce7c796e0ca5873ce5f3f6d40fe28398af
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-02-13 01:07:49 +0900

    bugfix: fix syntax error on completed_households.sql

--- api-server-wasm/schema/schema_completed_households.sql
+++ api-server-wasm/schema/schema_completed_households.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS completed_households;
 CREATE TABLE completed_households (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     year INTEGER NOT NULL,
-    month INTEGER NOT NULL,
-)
+    month INTEGER NOT NULL
+);
