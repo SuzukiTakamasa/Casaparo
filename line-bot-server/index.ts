36e4319fe749e2126eadfbf59a3220a9b4a5839c
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-02-22 01:35:43 +0900

    mod line-bot-server/index.ts

--- line-bot-server/index.ts
+++ line-bot-server/index.ts
@@ -1,9 +1,8 @@
 
-const LINE_MESSAGING_API_HOST = "https://api.line.me/v2/bot/message/broadcast"
-
 interface Env {
-    LINE_BOT_ACCESS_TOKEN: string
+    LINE_BOT_CHANNEL_ACCESS_TOKEN: string
     WORKER_RS_BACKEND_API_HOST: string
+    DATABASE_ENVIRONMENT: string
 }
 
 interface FixedAmount {
@@ -11,34 +10,106 @@ interface FixedAmount {
     total_amount: number
 }
 
-const getAPIHandler = async <T>(url: string, params?: string, headers?: object): Promise<T> => {
-    if (params) url += params
-    try {
-        const response = await fetch(url, {
-            method: 'GET'
-        })
-        return response.json()
-    } catch (e) {
-        throw new Error(`Failed to get: ${e}`)
+class LINEMessagingAPIHandler {
+    private lineBotHost: string
+    private backendHost: string
+    private lineBotHeaders: {[key: string]: string}
+    private backendHeaders: {[key: string]: string}
+    private currentYear: number
+    private currentMonth: number
+
+    constructor(env: Env) {
+        this.lineBotHost = "https://api.line.me/v2/bot/message/broadcast"
+        this.backendHost = env.WORKER_RS_BACKEND_API_HOST
+        this.lineBotHeaders = {
+            "Content-Type": "application/json",
+            "Authorization": `Bear ${env.LINE_BOT_CHANNEL_ACCESS_TOKEN}`
+        }
+        this.backendHeaders = {
+            "Content-Type": "application/json",
+            "Environment": env.DATABASE_ENVIRONMENT
+        }
+        this.currentYear = new Date().getFullYear()
+        this.currentMonth = new Date().getMonth() + 1
     }
-}
 
-const postAPIHandler = async <T>(url: string, data: object, headers: {[key: string]: string}): Promise<T> => {
-    try {
-        const res = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(data)
-        })
-        return await res.json()
-    } catch(e) {
-        throw new Error(`Failed to post: ${e}`)
+    public async _getAPIHandler<T>(url: string, params?: string): Promise<T> {
+        if (params) url += params
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: this.backendHeaders
+            })
+            return response.json()
+        } catch (e) {
+            throw new Error(`Failed to get: ${e}`)
+        }
     }
-}
 
-const broadcastFixedHousehold = async (env: Env) => {
-    const currentYear = new Date().getFullYear()
-    const currentMonth = new Date().getMonth() + 1
+    public async _postAPIHandler<T>(url: string, data: object): Promise<T> {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: this.lineBotHeaders,
+                body: JSON.stringify(data)
+            })
+            return await response.json()
+        } catch(e) {
+            throw new Error(`Failed to post: ${e}`)
+        }
+    }
 
-    const responseFixedHousehold = await getAPIHandler<FixedAmount>(`${env.WORKER_RS_BACKEND_API_HOST}/household/fixed_amount/${currentYear}/${currentMonth}`)
+    public async broadcastFixedHousehold() { 
+        const responseFixedHousehold = await this._getAPIHandler<FixedAmount>(`${this.backendHost}/household/fixed_amount/${this.currentYear}/${this.currentMonth}`)
+        const requestBody = {
+            message: [
+                {
+                    "type": "bubble",
+                    "direction": "ltr",
+                    "body": {
+                      "type": "box",
+                      "layout": "vertical",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "【今月の生活費のお知らせ】",
+                          "weight": "bold",
+                          "align": "center",
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `請求金額: ${responseFixedHousehold.total_amount}`,
+                          "contents": []
+                        },
+                        {
+                          "type": "text",
+                          "text": `生活費合計: ${responseFixedHousehold.billing_amount}`,
+                          "contents": []
+                        }
+                      ]
+                    },
+                    "footer": {
+                      "type": "box",
+                      "layout": "horizontal",
+                      "contents": [
+                        {
+                          "type": "button",
+                          "action": {
+                            "type": "message",
+                            "label": "支払い完了",
+                            "text": "支払い完了"
+                          },
+                          "color": "#201145FF",
+                          "height": "sm",
+                          "style": "primary",
+                          "gravity": "bottom"
+                        }
+                      ]
+                    }
+                  }
+            ]
+        }
+        await this._postAPIHandler<any>(this.lineBotHost, requestBody)
+    }
 }
