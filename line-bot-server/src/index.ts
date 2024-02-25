d57b2496e5b70260aa03971765c20f91ac2ccc83
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-02-25 16:18:56 +0900

    mod args

--- line-bot-server/src/index.ts
+++ line-bot-server/src/index.ts
@@ -4,7 +4,7 @@ import { Env } from "./api_handler"
 export default {
     async fetch(request: Request, env: Env, ctx: ExecutionContext) {
       if (request.url.endsWith('/reply_message')) {
-        return app.fetch(request)
+        return app.fetch(request, env, ctx)
       } else if (request.method === 'POST' && request.headers.has('x-cron-job-trigger')) {
         return await scheduled(request, env, ctx)
       } else {
