44d68c4944360a76c5cb0604bf170e2b43c1fa72
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-02-25 12:08:47 +0900

    separate index.ts

--- line-bot-server/src/index.ts
+++ line-bot-server/src/index.ts
@@ -1,25 +1,9 @@
-import { Hono } from "hono"
+import app from './webhook_handler'
 import LINEMessagingAPIHandler, { Env } from "./api_handler"
-
-const app = new Hono()
-
-app.post("/reply_message", async (ctx) => {
-    const env = {
-        LINE_BOT_CHANNEL_ACCESS_TOKEN: ctx.env!.LINE_BOT_CHANNEL_ACCESS_TOKEN,
-        WORKER_RS_BACKEND_API_HOST: ctx.env!.WORKER_RS_BACKEND_API_HOST,
-        DATABASE_ENVIRONMENT: ctx.env!.DATABASE_ENVIRONMENT
-    }
-    const apiHandler = new LINEMessagingAPIHandler(env as Env)
-    const result = await apiHandler.replyFixedHousehold(ctx)
-    return result
-})
 
 addEventListener('fetch', (event) => {
     event.respondWith(app.fetch(event.request))
-  })
-
-export { app }
-
+})
 
 export default {
     async scheduled(event: any, env: Env, _: ExecutionContext) {
---
+++ line-bot-server/src/webhook_handler.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono"
+import LINEMessagingAPIHandler, { Env } from "./api_handler"
+
+const app = new Hono()
+
+app.post("/reply_message", async (ctx) => {
+    const env = {
+        LINE_BOT_CHANNEL_ACCESS_TOKEN: ctx.env!.LINE_BOT_CHANNEL_ACCESS_TOKEN,
+        WORKER_RS_BACKEND_API_HOST: ctx.env!.WORKER_RS_BACKEND_API_HOST,
+        DATABASE_ENVIRONMENT: ctx.env!.DATABASE_ENVIRONMENT
+    }
+    const apiHandler = new LINEMessagingAPIHandler(env as Env)
+    const result = await apiHandler.replyFixedHousehold(ctx)
+    return result
+})
+
+export default app
