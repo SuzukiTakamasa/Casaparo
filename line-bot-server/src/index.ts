d20918cb6b1c81acae27aec14eb3033c495ea90d
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-01 02:24:50 +0900

    mod cron trigger

--- line-bot-server/src/index.ts
+++ line-bot-server/src/index.ts
@@ -1,7 +1,7 @@
 import LINEMessagingAPIHandler, { Env } from "./api_handler"
 
-
-const scheduled = async(event: any, env: Env) => {
+export default {
+  async scheduled(event: any, env: Env, ctx: ExecutionContext) {
     const apiHandler = new LINEMessagingAPIHandler(env)
     switch (event.cron) {
         case "0 0 19 * *":
@@ -14,19 +14,5 @@ const scheduled = async(event: any, env: Env) => {
             await apiHandler.completeHouseHold()
             break
     }
+  }
 }
-
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request: Request) {
-    return new Response('Cron job is executed...', {
-      headers: { 'content-type': 'text/plain' },
-    })
-  }
-  
-  export default {
-    fetch: handleRequest,
-    scheduled
-  }
