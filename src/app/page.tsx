95d295fc10fa2d0f7183d7ca0de26f909d4c958b
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-02 22:45:19 +0900

    mod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,12 +43,14 @@ export default function Home() {
    }, [fetchFixedAmount])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full rounded-lg overflow-hidden shadow-lg bg-white p-1">
-        <div className="bg-black text-white p-4">
-          <h2 className="text-x1 font-bold mb-4 text-center">🥺 今月の生活費・請求金額 🥺ྀི</h2>
-          <p className="text-base mb-2 text-right">{`生活費合計： ¥${totalAmount}`}</p>
-          <p className="text-base mb-2 text-right">{`請求金額： ¥${billingAmount}`}</p>
+    <main className="flex min-h-screen flex-col items-center justify-between py-4">
+      <div className="container max-w-full">
+        <div className="rounded-lg overflow-hidden shadow-lg bg-white p-1">
+          <div className="bg-black text-white p-2">
+            <h2 className="text-x1 font-bold mb-4 text-center">🥺 今月の生活費・請求金額 🥺ྀི</h2>
+            <p className="text-base mb-2 text-right">{`生活費合計： ¥${totalAmount}`}</p>
+            <p className="text-base mb-2 text-right">{`請求金額： ¥${billingAmount}`}</p>
+          </div>
         </div>
       </div>
     </main>
