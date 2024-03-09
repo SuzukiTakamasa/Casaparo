b865e995183e4db23a449ff934e303f761bc44a2
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-09 19:08:08 +0900

    mod component name Page

--- src/app/wiki/[id]/page.tsx
+++ src/app/wiki/[id]/page.tsx
@@ -1,31 +1,21 @@
-"use client"
-
 import APIClient from '@utils/api_client'
 import { WikiResponse } from '@utils/constants'
+import { useParams } from "next/navigation";
 
 const client = new APIClient()
 
-/*export const generateStaticParams = async() => {
+export const generateStaticParams = async() => {
     const wikis = await client.get<WikiResponse>('/wiki').then((res) => res)
     return wikis!.map((wiki) => ({
-        id: wiki.id
+        id: String(wiki.id)
     }))
 }
 
 
-const WikiDetail = ({ params }: {params: {id: number}}) => {
+const Page = ({ params }: {params: {id: string}}) => {
     return (
         <>{params.id}</>
     )
 }
 
-export default WikiDetail*/
-
-
-
-import { useParams } from "next/navigation";
-
-export default function WikiDetail() {
-  const { id } = useParams()
-  return <div>Post: {id}</div>;
-}
+export default Page
