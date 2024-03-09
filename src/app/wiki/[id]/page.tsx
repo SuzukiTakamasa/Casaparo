0b4675a7a8c405d6fb90ad10e722a658646f529d
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-09 18:39:02 +0900

    fix dynamic route err

--- src/app/wiki/[id]/page.tsx
+++ src/app/wiki/[id]/page.tsx
@@ -1,14 +1,18 @@
-"use client"
 
-import React, { useState, useEffect, useContext, useCallback } from 'react'
 import APIClient from '@utils/api_client'
-
+import { WikiResponse } from '@utils/constants'
 
 const client = new APIClient()
 
+export async function generateStaticParams() {
+    const wikis = await client.get<WikiResponse>('/wiki').then((res) => res)
+    return wikis!.map((wiki) => ({
+        id: wiki.id
+    }))
+}
 
-const WikiDetail = ({ params }: {params: {id: string}}) => {
-    
+
+const WikiDetail = ({ params }: {params: {id: number}}) => {
     return (
         <>{params.id}</>
     )
