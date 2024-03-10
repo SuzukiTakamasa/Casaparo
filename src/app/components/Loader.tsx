4b0ffb3ba7624c9447a58693a90af783babb4f7a
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-11 01:16:03 +0900

    add loader

---
+++ src/app/components/Loader.tsx
@@ -0,0 +1,16 @@
+import ClipLoader from 'react-spinners/ClipLoader'
+
+type LoaderProps = {
+    size: number
+    isLoading: boolean
+}
+
+const Loader = ({size, isLoading}: LoaderProps) => {
+    return (
+        <ClipLoader
+            color="#747474"
+            size={size}
+            loading={isLoading}
+        />
+    )
+}
