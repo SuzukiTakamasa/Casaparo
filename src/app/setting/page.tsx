8e47585e713bd129cfc1b43fcb2cd153e82d53b0
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-18 01:14:03 +0900

    add label to setting

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -4,7 +4,23 @@ import React from 'react'
 
 const Setting = () => {
     return (
+        <>
         <h1 className="text-2xl font-bold mc-4">🦵 設定 🦵</h1>
+
+        <div className="container mx-auto p-4">
+            <table className="table-auto min-w-full mt-4">
+                <thead>
+                    <tr>
+                        <th className="border-b-2 py-1">ラベル</th>
+                        <th className="border-b-2 py-1">ラベル名</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </div>
+        </>
     )
 }
 
