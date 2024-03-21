2fac9336e77dfd0206374e9a6e88e96414c060dc
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-03-21 23:59:31 +0900

    mod justify-center

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -373,7 +373,7 @@ const Schedule = () => {
                                 </label>
                             </div>
                         </div>
-                        <div className="flex justify-end space-x-4">
+                        <div className="flex justify-center space-x-4">
                             <button
                                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                 onClick={isUpdate ? handleUpdateSchedule : handleAddSchedule}
