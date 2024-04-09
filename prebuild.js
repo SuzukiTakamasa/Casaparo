9ea92ebec9bc8120c103d0d5412ee30bc91899f5
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-04-10 01:00:36 +0900

    update prebuild.js to add ./r2/src

--- prebuild.js
+++ prebuild.js
@@ -3,9 +3,15 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const dirPath = path.join(__dirname, './line-bot-server/src/')
+const lineborServerDirPath = path.join(__dirname, './line-bot-server/src/')
+const r2DirPath = path.join(__dirname, './r2/src/')
 
-fs.remove(dirPath, e => {
+fs.remove(lineborServerDirPath, e => {
     if (e) return console.error(e)
-    console.log(`${dirPath} was removed before the build.`)
+    console.log(`${lineborServerDirPath} was removed before the build.`)
+})
+
+fs.remove(r2DirPath, e => {
+    if (e) return console.error(e)
+    console.log(`${r2DirPath} was removed before the build.`)
 })
