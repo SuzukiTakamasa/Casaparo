e7a07a4412ebaec4c3c5ba3e654b19c17bcb468e
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-04-16 00:31:41 +0900

    mod cors setting of r2

--- r2/src/index.ts
+++ r2/src/index.ts
@@ -18,8 +18,8 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const corsHeaders = {
 			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
-			'Access-Control-Allow-Headers': 'Content-Type, application/octet-stream',
+			'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
 			'Access-Control-Max-Age': '86400',
 		}
 		if (request.method === 'OPTIONS') {
