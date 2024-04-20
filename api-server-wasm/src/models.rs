efbaf83e3ee4da57eec78cb0107e5d266f67bded
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-04-20 15:55:50 +0900

    bugfix: mod image_url nullable

--- api-server-wasm/src/models.rs
+++ api-server-wasm/src/models.rs
@@ -61,7 +61,7 @@ pub struct Wikis {
     pub content: String,
     pub created_by: u8,
     pub updated_at: String,
-    pub image_url: String,
+    pub image_url: Option<String>,
     pub version: u32
 }
 
