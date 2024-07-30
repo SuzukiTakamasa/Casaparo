acaabc17b82de3cf0dbb5623c18469ed1215cc68
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-07-31 00:12:51 +0900

    add create_wiki

--- api-server-wasm/src/infrastructure/repositories/d1_wiki_repository.rs
+++ api-server-wasm/src/infrastructure/repositories/d1_wiki_repository.rs
@@ -36,7 +36,18 @@ impl WikiRepository for D1WikiRepository {
     }
 
     async fn create_wiki(&self, wiki: &Wikis) -> Result<(), Error> {
-
+        let statement = self.d1.prepare("insert into wikis (title, content, created_by, updated_at, image_url, version) values (?1, ?2, ?3, ?4, ?5, ?6)");
+        let query = statement.bind(&[wiki.title.into(),
+                                     wiki.content.into(),
+                                     wiki.created_by.into(),
+                                     wiki.updated_at.into(),
+                                     wiki.image_url.into(),
+                                     wiki.version.into()])?;
+        
+        let result = match query.run().await {
+            Ok(res) => Response::ok("A wiki was created"),
+            Err(e) => Response::error("QUery failed", 500)
+        };
     }
 
     async fn update_wiki(&self, wiki: &Wikis) -> Result<(), Error> {
