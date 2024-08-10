fb408f13303fb89b7332014877a09dfb909eacc4
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-08-10 10:51:21 +0900

    remove EntityManager, cancel implementing it to Wiki, and declare optimistic_locking macro

--- api-server-wasm/src/domain/entities/service.rs
+++ api-server-wasm/src/domain/entities/service.rs
@@ -5,6 +5,22 @@ pub struct LatestVersion {
     pub version: u32
 }
 
-pub trait EntityManager {
-    fn increment_version(&mut self);
+
+#[macro_export]
+macro_rules! optimistic_locking {
+    ($entity:expr, $fetch_version_result:expr) => {{
+        match $fetch_version_result {
+            Some(latest) => {
+                if $entity.version == latest.version {
+                    $entity.version += 1;
+                    Ok(())
+                } else {
+                    Err(worker::Error::RustError("Attempt to update a stale object".to_string()))
+                }
+            },
+            None => Err(worker::Error::RustError("Version is found None".to_string()))
+        }
+    }};
 }
+
+pub use optimistic_locking;
