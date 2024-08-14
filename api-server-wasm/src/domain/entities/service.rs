cad95f73f563304871b4d9672b7f52b6e249aef6
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-08-14 16:21:10 +0900

    cancel to define optimistic_locking function

--- api-server-wasm/src/domain/entities/service.rs
+++ api-server-wasm/src/domain/entities/service.rs
@@ -4,23 +4,3 @@ use serde::{Deserialize, Serialize};
 pub struct LatestVersion {
     pub version: u32
 }
-
-
-#[macro_export]
-macro_rules! optimistic_locking {
-    ($entity:expr, $fetch_version_result:expr) => {{
-        match $fetch_version_result {
-            Some(latest) => {
-                if $entity.version == latest.version {
-                    $entity.version += 1;
-                    Ok(())
-                } else {
-                    Err(worker::Error::RustError("Attempt to update a stale object".to_string()))
-                }
-            },
-            None => Err(worker::Error::RustError("Version is found None".to_string()))
-        }
-    }};
-}
-
-pub use optimistic_locking;
