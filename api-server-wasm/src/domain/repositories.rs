83ae6b2aa09fd92820931f0af0e748760aa6e543
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-08-22 01:12:51 +0900

    add schedule repository

--- api-server-wasm/src/domain/repositories.rs
+++ api-server-wasm/src/domain/repositories.rs
@@ -1,3 +1,4 @@
+pub mod schedule_repository;
 pub mod wiki_repository;
 pub mod label_repository;
 pub mod anniversary_repository;
---
+++ api-server-wasm/src/domain/repositories/schedule_repository.rs
@@ -0,0 +1,12 @@
+use crate::domain::entities::schedule::Schedules;
+use crate::async_trait::async_trait;
+use worker::Result;
+
+#[async_trait(?Send)]
+pub trait ScheduleRepository {
+    async fn get_schedules(&self) -> Result<Vec<Schedules>>;
+    async fn get_today_or_tomorrow_schedules(&self, year: u16, month: u8, date: u8) -> Result<Vec<Schedules>>;
+    async fn create_schedule(&self, schedule: &Schedules) -> Result<()>;
+    async fn update_schedule(&self, schedule: &Schedules) -> Result<()>;
+    async fn delete_schedule(&self, schedule: &Schedules) -> Result<()>;
+}
