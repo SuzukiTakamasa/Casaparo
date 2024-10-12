81cdba8d2a189d68cadf7db3f1fb781760f185b1
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-10-13 01:28:50 +0900

    add TextLinkProps

---
+++ src/app/components/TextLink.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import { ArrowRightStartToIcon } from './HeroicIcons'
+
+type TextLinkProps = {
+    path: string,
+    text: string
+}
+
+const TextLink = ({ path, text }: TextLinkProps) => {
+    return (
+        <Link href={path} className="flex text-xl text-blue-500 font-bold hover:underline">
+            <ArrowRightStartToIcon />
+            {text}
+        </Link>
+    )
+}
+
+export default TextLink
