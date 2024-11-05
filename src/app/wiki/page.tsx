f5ae6b5ab68aae7d3b7cc80074fb0b5b702573d8
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-11-06 00:30:06 +0900

    remove <WikiResponse> from post method

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -91,7 +91,7 @@ const Wiki = () => {
             image_url: imageUrl,
             version: version
         }
-        await client.post<WikiResponse>('/v2/wiki/create', addedWikiData)
+        await client.post('/v2/wiki/create', addedWikiData)
         await fetchWikis()
     }
     const updateWiki = async () => {
@@ -104,7 +104,7 @@ const Wiki = () => {
             image_url: imageUrl,
             version: version
         }
-        await client.post<WikiResponse>('/v2/wiki/update', updatedWikiData)
+        await client.post('/v2/wiki/update', updatedWikiData)
         await fetchWikis()
     }
     const updateImageUrlEmpty = async () => {
@@ -117,12 +117,12 @@ const Wiki = () => {
             image_url: "",
             version: version
         }
-        await client.post<WikiResponse>('/v2/wiki/update', updatedWikiData)
+        await client.post('/v2/wiki/update', updatedWikiData)
         await fetchWikis()
     }
     const deleteWiki = async(deleteWikiData: WikiData) => {
         if (!window.confirm("削除しますか？")) return
-        await client.post<WikiResponse>('/v2/wiki/delete', deleteWikiData)
+        await client.post('/v2/wiki/delete', deleteWikiData)
         await fetchWikis()
     }
     const handleUploadFile = async(event: React.ChangeEvent<HTMLInputElement>) => {
