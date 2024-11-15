675badd81ca604efaf96807c3c4be1fbb61a5961
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-11-15 22:10:06 +0900

    add task entities to backend

---
+++ api-server-wasm/src/domain/entities/task.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tasks {
+    pub id: Option<u32>,
+    pub title: String,
+    pub status: u8,
+    pub priority: u8,
+    pub description: String,
+    pub created_by: u8,
+    pub due_date: String,
+    pub is_sub_task: u8,
+    pub parent_task_id: u32,
+    pub version: u32
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TaskComments {
+    pub id: Option<u32>,
+    pub created_by: u8,
+    pub created_at: String,
+    pub comment: String,
+    pub task_id: u32,
+    pub version: u32
+}
