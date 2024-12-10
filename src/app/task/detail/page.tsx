f0347b8b8ffcb4b31dff8700d97c7c200cae7d00
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2024-12-11 00:16:54 +0900

    add my-1 between each task comment

--- src/app/task/detail/page.tsx
+++ src/app/task/detail/page.tsx
@@ -510,7 +510,7 @@ const TaskDetail = () => {
                 </button>
                 </div>
                 {taskComments.map((taskComment, i) => (
-                    <>
+                    <div className="my-1">
                         <div key={i} className="flex justify-left space-x-2">
                             <div className="text-lg">{setUser(taskComment.created_by)}</div>
                             <div className="rounded-lg overflow-hidden shadow-lg bg-green-500 p-1">
@@ -546,7 +546,7 @@ const TaskDetail = () => {
                                 </div>
                             </div>
                         </div>
-                    </>
+                    </div>
                 ))}
             </div>
             {showTaskCommentDialog && (
