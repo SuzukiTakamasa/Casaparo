8cc199b583699409ce5b07b081899ca0cdffc3cc
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-01-09 00:45:19 +0900

    mod next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: `export`
-    //distDir: 'out'
+    output: 'export',
     images: {
+        unoptimized: true,
         domains: ['r2-dev.incubus-appalachia.workers.dev', 'r2.incubus-appalachia.workers.dev'],
     }
 }
