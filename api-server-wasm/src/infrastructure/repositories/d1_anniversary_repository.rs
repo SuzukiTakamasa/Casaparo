635db818b27e5af010cbbadd19bd1e197aef29f5
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-02-04 23:50:57 +0900

    adapt law string literal to d1_anniversary_repository as a test

--- api-server-wasm/src/infrastructure/repositories/d1_anniversary_repository.rs
+++ api-server-wasm/src/infrastructure/repositories/d1_anniversary_repository.rs
@@ -18,13 +18,17 @@ impl D1AnniversaryRepository {
 #[async_trait(?Send)]
 impl AnniversaryRepository for D1AnniversaryRepository {
     async fn get_anniversaries(&self) -> Result<Vec<Anniversaries>> {
-        let query = self.db.prepare("select * from anniversaries order by id desc");
+        let query = self.db.prepare(r#"select *
+                                                            from anniversaries
+                                                            order by id desc"#);
         let result = query.all().await?;
         result.results::<Anniversaries>()
     }
 
     async fn create_anniversary(&self, anniversary: &Anniversaries) -> Result<()> {
-        let statement = self.db.prepare("insert into anniversaries (month, date, description, version) values (?1, ?2, ?3, ?4)");
+        let statement = self.db.prepare(r#"insert into anniversaries
+                                                                (month, date, description, version)
+                                                                values (?1, ?2, ?3, ?4)"#);
         let query = statement.bind(&[anniversary.month.into(),
                                                           anniversary.date.into(),
                                                           anniversary.description.clone().into(),
@@ -34,7 +38,9 @@ impl AnniversaryRepository for D1AnniversaryRepository {
     }
 
     async fn update_anniversary(&self, anniversary: &mut Anniversaries) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from anniversaries where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from anniversaries
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[anniversary.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -46,7 +52,9 @@ impl AnniversaryRepository for D1AnniversaryRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("update anniversaries set month = ?1, date = ?2, description = ?3, version = ?4 where id = ?5");
+        let statement = self.db.prepare(r#"update anniversaries
+                                                                set month = ?1, date = ?2, description = ?3, version = ?4
+                                                                where id = ?5"#);
         let query = statement.bind(&[anniversary.month.into(),
                                                           anniversary.date.into(),
                                                           anniversary.description.clone().into(),
@@ -57,7 +65,9 @@ impl AnniversaryRepository for D1AnniversaryRepository {
     }
 
     async fn delete_anniversary(&self, anniversary: &mut Anniversaries) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from anniversaries where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from anniversaries
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[anniversary.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -69,7 +79,9 @@ impl AnniversaryRepository for D1AnniversaryRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("delete from anniversaries where id = ?1");
+        let statement = self.db.prepare(r#"delete from
+                                                                anniversaries
+                                                                where id = ?1"#);
         let query = statement.bind(&[anniversary.id.into()])?;
         query.run().await?;
         Ok(())
