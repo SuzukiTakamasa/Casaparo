3198ca62c75584fa9558b51c5cc1ea794dc9236f
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-02-07 23:13:09 +0900

    apply raw string literal to sql query

--- api-server-wasm/src/infrastructure/repositories/d1_task_comment_repository.rs
+++ api-server-wasm/src/infrastructure/repositories/d1_task_comment_repository.rs
@@ -18,14 +18,18 @@ impl D1TaskCommentRepository {
 #[async_trait(?Send)]
 impl TaskCommentRepository for D1TaskCommentRepository {
     async fn get_task_comments_by_task_id(&self, task_id: u32) -> Result<Vec<TaskComments>> {
-        let statement = self.db.prepare("select * from task_comments where task_id = ?1");
+        let statement = self.db.prepare(r#"select *
+                                                                from task_comments
+                                                                where task_id = ?1"#);
         let query = statement.bind(&[task_id.into()])?; 
         let result = query.all().await?;
         result.results::<TaskComments>()
     }
 
     async fn create_task_comment(&self, task_comment: &TaskComments) -> Result<()> {
-        let statement = self.db.prepare("insert into task_comments (created_by, updated_at, comment, task_id, version) values (?1, ?2, ?3, ?4, ?5)");
+        let statement = self.db.prepare(r#"insert into task_comments
+                                                                (created_by, updated_at, comment, task_id, version)
+                                                                values (?1, ?2, ?3, ?4, ?5)"#);
         let query = statement.bind(&[task_comment.created_by.into(),
                                                           task_comment.updated_at.clone().into(),
                                                           task_comment.comment.clone().into(),
@@ -36,7 +40,9 @@ impl TaskCommentRepository for D1TaskCommentRepository {
     }
 
     async fn update_task_comment(&self, task_comment: &mut TaskComments) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from task_comments where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from task_comments
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[task_comment.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -48,7 +54,13 @@ impl TaskCommentRepository for D1TaskCommentRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("update task_comments set created_by = ?1, updated_at = ?2, comment = ?3, task_id = ?4, version = ?5 where id = ?6");
+        let statement = self.db.prepare(r#"update task_comments
+                                                                set created_by = ?1,
+                                                                updated_at = ?2,
+                                                                comment = ?3,
+                                                                task_id = ?4,
+                                                                version = ?5
+                                                                where id = ?6"#);
         let query = statement.bind(&[task_comment.created_by.into(),
                                                           task_comment.updated_at.clone().into(),
                                                           task_comment.comment.clone().into(),
@@ -60,7 +72,9 @@ impl TaskCommentRepository for D1TaskCommentRepository {
     }
 
     async fn delete_task_comment(&self, task_comment: &mut TaskComments) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from task_comments where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from task_comments
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[task_comment.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -72,7 +86,9 @@ impl TaskCommentRepository for D1TaskCommentRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("delete from task_comments where id = ?1");
+        let statement = self.db.prepare(r#"delete
+                                                                from task_comments
+                                                                where id = ?1"#);
         let query = statement.bind(&[task_comment.id.into()])?;
         query.run().await?;
         Ok(())
--- api-server-wasm/src/infrastructure/repositories/d1_wiki_repository.rs
+++ api-server-wasm/src/infrastructure/repositories/d1_wiki_repository.rs
@@ -18,13 +18,17 @@ impl D1WikiRepository {
 #[async_trait(?Send)]
 impl WikiRepository for D1WikiRepository {
     async fn get_wikis(&self) -> Result<Vec<Wikis>> {
-        let query = self.db.prepare("select * from wikis order by id desc");
+        let query = self.db.prepare(r#"select *
+                                                            from wikis
+                                                            order by id desc"#);
         let result = query.all().await?;
         result.results::<Wikis>()
     }
 
     async fn get_wiki_by_id(&self, id: u32) -> Result<Wikis> {
-        let statement = self.db.prepare("select * from wikis where id = ?1");
+        let statement = self.db.prepare(r#"select *
+                                                                from wikis
+                                                                where id = ?1"#);
         let query = statement.bind(&[id.into()])?;
         let result = query.first::<Wikis>(None).await?;
         match result {
@@ -34,7 +38,9 @@ impl WikiRepository for D1WikiRepository {
     }
 
     async fn create_wiki(&self, wiki: &Wikis) -> Result<()> {
-        let statement = self.db.prepare("insert into wikis (title, content, created_by, updated_at, image_url, version) values (?1, ?2, ?3, ?4, ?5, ?6)");
+        let statement = self.db.prepare(r#"insert into wikis
+                                                                (title, content, created_by, updated_at, image_url, version)
+                                                                values (?1, ?2, ?3, ?4, ?5, ?6)"#);
         let query = statement.bind(&[wiki.title.clone().into(),
                                                           wiki.content.clone().into(),
                                                           wiki.created_by.into(),
@@ -47,7 +53,9 @@ impl WikiRepository for D1WikiRepository {
     }
 
     async fn update_wiki(&self, wiki: &mut Wikis) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from wikis where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from wikis
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[wiki.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -59,7 +67,14 @@ impl WikiRepository for D1WikiRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("update wikis set title = ?1, content = ?2, created_by = ?3, updated_at = ?4, image_url = ?5, version = ?6 where id = ?7");
+        let statement = self.db.prepare(r#"update wikis
+                                                                set title = ?1,
+                                                                content = ?2,
+                                                                created_by = ?3,
+                                                                updated_at = ?4,
+                                                                image_url = ?5,
+                                                                version = ?6
+                                                                where id = ?7"#);
         let query = statement.bind(&[wiki.title.clone().into(),
                                                           wiki.content.clone().into(),
                                                           wiki.created_by.into(),
@@ -72,7 +87,9 @@ impl WikiRepository for D1WikiRepository {
     }
 
     async fn delete_wiki(&self, wiki: &mut Wikis) -> Result<()> {
-        let fetch_version_statement = self.db.prepare("select version from wikis where id = ?1");
+        let fetch_version_statement = self.db.prepare(r#"select version
+                                                                              from wikis
+                                                                              where id = ?1"#);
         let fetch_version_query = fetch_version_statement.bind(&[wiki.id.into()])?;
         let fetch_version_result = fetch_version_query.first::<LatestVersion>(None).await?;
         if let Some(latest) = fetch_version_result {
@@ -84,7 +101,9 @@ impl WikiRepository for D1WikiRepository {
         } else {
             return Err(worker::Error::RustError("Version is found None".to_string()))
         }
-        let statement = self.db.prepare("delete from wikis where id = ?1");
+        let statement = self.db.prepare(r#"delete
+                                                                from wikis
+                                                                where id = ?1"#);
         let query = statement.bind(&[wiki.id.into()])?;
         query.run().await?;
         Ok(())
