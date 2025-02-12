9511e277fd68a1af0b91358d32c153fa064f5cdc
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-02-13 01:00:28 +0900

    add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+})
+const nextConfig = withPWA({
     output: 'export',
     images: {
         unoptimized: true,
         domains: ['r2-dev.incubus-appalachia.workers.dev', 'r2.incubus-appalachia.workers.dev'],
     }
-}
+})
 
 module.exports = nextConfig
