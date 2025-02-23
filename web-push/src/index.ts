5b6718659ddeacba947192136e1ef3765259a53a
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-02-23 22:42:33 +0900

    add api_handler.ts to web-push project

--- web-push/src/index.ts
+++ web-push/src/index.ts
@@ -1,27 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 import webpush from 'web-push'
+import APIHandler from './api_handler'
 
-interface Env {
+export interface Env {
 	VAPID_PUBLIC_KEY: string
 	VAPID_PROVATE_KEY: string
-	BACKEND_HOST_NAME: string
+	WORKER_RS_BACKEND_API_HOST: string
 }
 
-interface WebPushSubscription {
+export interface WebPushSubscription {
 	id?: number
-	user_id: string,
+	subscription_id: string,
 	endpoint: string,
 	p256h_key: string,
 	auth_key: string,
@@ -31,13 +20,14 @@ interface WebPushSubscription {
 	version: number
 }
 
-interface BroadcastPayload {
+export interface BroadcastPayload {
 	title: string
 	body: string
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const api_handler = new APIHandler(env)
 		if (request.method === 'POST' && new URL(request.url).pathname === '/subscribe') {
 			const subscription = await request.json()
 		} else if (request.method === 'POST' && new URL(request.url).pathname === '/unsubscribe') {
@@ -45,6 +35,6 @@ export default {
 		} else if (request.method === 'POST' && new URL(request.url).pathname === '/broadcast') {
 			const payload = await request.json()
 		}
-		return new Response('not found', { status: 404})
+		return new Response('not found', { status: 404 })
 	},
 }
