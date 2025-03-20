e95d0e2360111aca13ccae3a1ef4cf6cc73d8a36
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-03-21 01:00:47 +0900

    fix web_push_subscriptions.sql schema

--- api-server-wasm/schema/schema_web_push_subscriptions.sql
+++ api-server-wasm/schema/schema_web_push_subscriptions.sql
@@ -1,7 +1,8 @@
+DROP TABLE IF EXISTS web_push_subscriptions;
 CREATE TABLE web_push_subscriptions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     endpoint TEXT UNIQUE NOT NULL,
-    p256dh_key DEFAULT NULL,
+    p256h_key DEFAULT NULL,
     auth_key DEFAULT NULL,
     version INTEGER NOT NULL
 )
