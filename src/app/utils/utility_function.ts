f5c651a785e6aca0304ede0fc9a5f082e24ef07a
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-03-30 23:56:07 +0900

    add a countermeasure for SSR

--- src/app/utils/utility_function.ts
+++ src/app/utils/utility_function.ts
@@ -188,6 +188,9 @@ export const setPriorityStr = (priority: number): string => {
 }
 
 export const urlBase64ToUint8Array = (base64String: string): Uint8Array => {
+    if (typeof window === 'undefined') {
+        return new Uint8Array() // Return an empty Uint8Array if window is not defined (e.g. during SSR)
+      }
     const padding = '='.repeat((4 - base64String.length % 4) % 4)
     const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
     const rawData = window.atob(base64)
