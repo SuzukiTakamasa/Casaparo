59d9e45282676a20b6f1ce8278e7e0bbdfc11a64
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2025-05-28 00:48:15 +0900

    remove unnecessary patch and add with_status to JSONResponse::new

--- src/app/patch/patch_list.ts
+++ src/app/patch/patch_list.ts
@@ -5,44 +5,7 @@ const client  = new APIClient()
 
 
 const patchList: Patches<any>[] = [
-    {
-        id: 1,
-        description: "Decode descriptions of wiki",
-        function: async () => {
-            const response = await client.get<WikiResponse>("/v2/wiki")
-            if (response.error) return { data: null, error: response.error }
-            if (!response.data) return { data: null, error: "No data found" }
-            const wikis = response.data!
-            for (const wiki of wikis) {
-                wiki.content = decodeURI(wiki.content)
-            }
-            const result = await Promise.all(wikis?.map(async (data) => {
-                return client.post<WikiData>("/v2/wiki/update", data)
-            }))
-            if (result.every(res => res.error === null)) {
-                return { data: result, error: null}
-            }
-        }
-    },
-    {
-        id: 2,
-        description: "Decode descriptions of task",
-        function: async () => {
-            const response = await client.get<TaskResponse>("/v2/task")
-            if (response.error) return { data: null, error: response.error }
-            if (!response.data) return { data: null, error: "No data found" }
-            const tasks = response.data!
-            for (const task of tasks) {
-                task.description = decodeURI(task.description)
-            }
-            const result = await Promise.all(tasks?.map(async (data) => {
-                return client.post<TaskData>("/v2/task/update", data)
-            }))
-            if (result.every(res => res.error === null)) {
-                return { data: result, error: null}
-            }
-        }
-    }
+
 ]
 
 export default patchList
